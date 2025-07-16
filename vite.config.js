e86d10d609b5d1d3a52124595268137f9c5db793
DawoonJeong <wjdekgb@gmail.com>
2025-07-16 16:38:06 +0900

    백엔드

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,             // ✅ 외부 접속 허용을 위해 추가
+    port: 5173,             // 생략 가능 (기본값)
     proxy: {
-      '/api': 'http://localhost:5000',  // ✅ Flask 서버 주소
+      '/api': 'http://localhost:5000',
       '/upload_output_json': 'http://localhost:5000',
-      '/upload_csv': 'http://localhost:5000',  // ✅ 이 줄 추가!
+      '/upload_csv': 'http://localhost:5000',
     }
   }
 })
